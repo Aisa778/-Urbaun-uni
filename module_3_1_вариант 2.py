ac6899a2c08b5878b06ebefcb1d113b9a1e1bf03
Aisa778 <aisatryskova1981@gmail.com>
2024-09-22 21:03:47 +0300

    Add files via upload

---
+++ module_3_1_вариант 2.py
@@ -0,0 +1,32 @@
+calls = 0
+
+def string_info():
+    global calls
+    str_1=input('Введите название любого города в России:')
+    cort_1=len(str_1)
+    cort_2=str_1.upper()
+    cort_3 = str_1.lower()
+    all_cort = (cort_1,cort_2,cort_3)
+    print(all_cort)
+    calls+=1
+string_info()
+
+def is_contains():
+    global calls
+    # list_to_search=[]
+    string = input('Введите день недели:').lower()
+    a_1 = input('Какой сегодня день недели?').lower()
+    a_2= input('Как вас зовут?')
+    a_3= str(input('Cколько Вам лет?'))
+    list_to_search=[a_1,a_2,a_3]
+    if string in list_to_search:
+        a_4='True'
+    else:
+        a_4='False'
+    low_1=a_1.upper()
+    print(a_1,low_1,a_2,a_3)
+    print (a_4)
+    calls+=1
+is_contains()
+
+print(calls)
