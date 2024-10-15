c07fa1f9997296857651f2a560b0341c0b98e7b8
Aisa778 <aisatryskova1981@gmail.com>
2024-10-15 22:09:44 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,77 @@
+new_floor= int(input('Введите номер нужного Вам этажа '))
+
+class House:
+    def __init__(self,name, number_of_floors):
+        pass
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self,new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            return print('Такого этажа не существует')
+        else:
+            for i in range(1,new_floor+1):
+                print(i)
+
+    def __len__(self):
+        print('Всего этажей -', self.number_of_floors)
+    def __str__(self):
+        print('С уважением, коллектив', self.name)
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.number_of_floors==other.number_of_floors
+        else:
+            print ('нельзя сравнить')
+
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+            print ('нельзя сравнить')
+
+    def __ne__( self, other):
+        return self.number_of_floors != other.number_of_floors
+    def __add__(self, value):
+        return self.number_of_floors + value
+
+    def __radd__(self, value):
+        return self.number_of_floors + value
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            return self.number_of_floors + value
+        else:
+            print ('введите число')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('ЖК Венеция', 25)
+# h1.go_to(new_floor)
+# h2.go_to(new_floor)
+
+h1.__len__()
+h1.__str__()
+h2.__len__()
+h2.__str__()
+
+print(h1==h2)
+print(h1<h2)
+print(h1<=h2)
+print(h1>h2)
+print(h1>=h2)
+print(h1!=h2)
+print('Сейчас в ЖК Горский', h1.__add__(2), 'этажей, а было', h1.number_of_floors, 'этажей' )
+print('Сейчас в ЖК Венеция', h2.__add__(2), 'этажей, а было', h2.number_of_floors, 'этажей' )
+print(h1.__radd__(1))
+print(h2.__radd__(3))
+print(h1.__iadd__('единица'))
+print(h2.__iadd__(3))
