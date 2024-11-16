0be02af9b81c4c14a3ce8b88e837f4eea8e39443
Aisa778 <aisatryskova1981@gmail.com>
2024-11-16 12:09:49 +0300

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -5,21 +5,10 @@ def add_everything_up(a,b):
         x = a + b
         print(f'сложение выполнено, суммa равна:{x}')
     except TypeError:
-        if type(a) == str and type(b) == int:
-            x = a + str(b)
-            print(f'сложение строк выполнено, сумма равна:, {x}')
-        elif type(a) == str and type(b) == float:
-            x = a + str(b)
-            print(f'сложение строк выполнено, сумма равна:, {x}')
-        elif type(a) == int and type(b) == str:
-            x = str(a) + b
-            print(f'сложение строк выполнено, сумма равна:, {x}')
-        elif type(a) == float and type(b) == str:
-            x = str(a) + b
-            print(f'сложение строк выполнено, сумма равна:, {x}')
-            # print(f'сложение не выполнено')
-        else:
-            print('Сложение не может быть выполнено')
+        x=str(a)+str(b)
+
+        print(f'сложение строк выполнено, сумма равна:, {x}')
+        
 
 add_everything_up('aa',2)
 add_everything_up(123.456, 'строка')
