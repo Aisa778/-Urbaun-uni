5e3bf72256300f766b0e6424558ef993cbe55ef9
Aisa778 <aisatryskova1981@gmail.com>
2024-12-04 09:22:52 +0300

    Add files via upload

--- module_9_7.py
+++ module_9_7.py
@@ -3,19 +3,23 @@ def is_prime(sum_tree):
 
     def wrapper(*args):
         func_one = sum_tree(*args)
+        if func_one > 1:
+            z=0
+            for j in range(2,func_one):
 
-        z=0
-        for j in range(2,func_one):
+                if func_one % j==0:
+                    z+=1
+                else:
+                    j+=1
 
-            if func_one % j==0:
-                z+=1
+            if z == 0:
+                print ('простое число')
             else:
-                j+=1
-
-        if z == 0:
-            print ('простое число')
-        else:
-            print('не простое число')
+                print('составное число')
+        elif func_one <= 1:
+            print('число меньше или равное 1 не может быть простым')
+        # elif func_one ==0:
+        #     print('число 0 не является простым или составным')
 
         return func_one
     return wrapper
@@ -31,7 +35,7 @@ def sum_tree(*args):
 
 
 
-a = sum_tree(2,3,6)
+a = sum_tree(2,2,4)
 print(a)
 
 
